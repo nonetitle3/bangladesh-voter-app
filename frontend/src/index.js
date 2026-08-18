e2a2cd4a43e1c3daf2d72efcb69b7dcb8dac1856
nonetitle3 <nonetitle3@gmail.com>
2026-08-18 14:24:19 +0600

    Add /admin route for React admin panel

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import AdminPanel from './AdminPanel';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const isAdmin = window.location.pathname.replace(/\/$/, '') === '/admin';
+
 root.render(
   <React.StrictMode>
-    <App />
+    {isAdmin ? <AdminPanel /> : <App />}
   </React.StrictMode>
 );
